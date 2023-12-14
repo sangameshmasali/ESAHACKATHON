43ca85552b7562cecef089cf01c67b358b111643
Abhishek Kumar <abhishek.kumar@ecolab.com>
2023-12-14 17:51:22 +0530

    Testing checking

--- assetmanagement/assetmanagement/Controllers/WeatherForecastController.cs
+++ assetmanagement/assetmanagement/Controllers/WeatherForecastController.cs
@@ -15,7 +15,7 @@ namespace assetmanagement.Controllers
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
+        //Test code
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
